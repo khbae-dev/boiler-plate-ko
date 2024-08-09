945b41b7f7af4e7916b3df42728b995eeaf2df12
RoyBae <52058395+baeSoft@users.noreply.github.com>
2024-08-10 01:11:21 +0900

    feat: Add post /register

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://khbaedev:rkdgus01!@boilerplate.rygsb63.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate', {
@@ -10,6 +17,16 @@ mongoose.connect('mongodb+srv://khbaedev:rkdgus01!@boilerplate.rygsb63.mongodb.n
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    const result = user.save().then(() => {
+        res.status(200).json({
+        success: true
+        })
+    }).catch((err) => res.json({sucess: false, err}))
 })
 
 app.listen(port, () => {
