768561d6f4343b41e79c17ea23d6b9f0a57ac3c6
RoyBae <52058395+baeSoft@users.noreply.github.com>
2024-08-11 02:08:25 +0900

    feat: Add config mongo uri

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 4000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
+const config = require('./config/key');
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,7 +11,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://khbaedev:rkdgus01!@boilerplate.rygsb63.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate', {
+
+mongoose.connect(config.mongoUri, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
